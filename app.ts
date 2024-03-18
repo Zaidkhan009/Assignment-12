5e22fa5d0111ea5dba92543e0257ea86a2d3802e
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-19 01:53:44 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,5 @@
+// let members: string[] =['Zaid', 'Azeem', 'Huzaifa' , 'Atif ' , 'Umer'];
+// let message: string='kal birthday hai kis ki:';
+// for(let i=0; i<members.length; i++){
+//      console.log(message + members[i]);
+//  }
